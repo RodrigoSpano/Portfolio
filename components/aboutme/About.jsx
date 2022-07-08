430576fcace422211006ecdefa26b5d18564e67d
RodrigoSpano <rorrospano17@gmail.com>
2022-07-08 16:08:25 -0300

    a seguir

--- components/aboutme/About.jsx
+++ components/aboutme/About.jsx
@@ -1,9 +1,14 @@
 import { Stack } from '@chakra-ui/react'
 import React from 'react'
+import Curriculum from './Curriculum'
+import Me from './Me'
 
 function About() {
   return (
-    <Stack h='100vh' bgColor='gray.800'></Stack>
+    <Stack h='100vh' bgColor='gray.800' direction='row' alignItems='center' justifycontent='center'>
+      <Curriculum />
+      <Me />
+    </Stack>
   )
 }
 
