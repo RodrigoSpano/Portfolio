d31b4d76ffdea6173eea359a2531eda051f0a554
RodrigoSpano <rorrospano17@gmail.com>
2022-07-08 14:49:48 -0300

    animation cool

--- components/header/Header.jsx
+++ components/header/Header.jsx
@@ -9,11 +9,11 @@ function Header() {
     <Stack overflow='hidden' minH='60vh' alignItems='center' justifyContent='center' >
 
       <Stack>
-        <motion.div transition={{ duration: 3 }} animate={{ x: [-1000, 0] }}>
+        <motion.div transition={{ duration: 3 }} initial={{x: '-100vw'}} animate={{ x: 0}}>
           <Heading as='h3' fontFamily='Comfortaa, cursive' fontSize='md' color='gray.200' pl={1}>Hi, I am</Heading>
         </motion.div>
 
-        <motion.div transition={{ duration: 3}} animate={{ x: [1000, 0] }} >
+        <motion.div transition={{ duration: 3}} initial={{x: '100vw'}} animate={{ x: 0 }} >
           <Heading as='h1' fontSize='7xl' color='orange.400' fontFamily='Secular One, sans-serif'>Rodrigo Spano</Heading>
         </motion.div>
 
