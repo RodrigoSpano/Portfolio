c09b286498ec31d3c7acdf26c7bc73e207114e86
RodrigoSpano <rorrospano17@gmail.com>
2022-07-10 19:17:35 -0300

    animation done

--- components/aboutme/Me.jsx
+++ components/aboutme/Me.jsx
@@ -1,29 +1,50 @@
 import { BsDownload } from 'react-icons/bs'
 import { Avatar, Flex, Heading, Link, Stack, Text } from '@chakra-ui/react'
+import { useInView } from 'react-intersection-observer'
+import { useEffect } from 'react'
+import { useAnimation, motion } from 'framer-motion'
 
 function Me() {
+
+  const { ref, inView } = useInView()
+  const animation = useAnimation()
+
+  useEffect( () => {
+    if(inView){
+      animation.start({
+        x:0,
+        transition: { type:'spring' ,delay: 0.2, duration: 1 , bounce: 0.5 }
+      })
+    }
+    if(!inView){
+      animation.start({ x: '40vw'})
+    }
+  }, [inView] )
+
   return (
-    <Stack w='xl'>
-      <Heading as='h2' textAlign='center' fontFamily='Teko, sans-serif, cursive' color='orange.400' fontSize='6xl' >About me</Heading>
-      
-      <Stack p={5} borderRadius={20} bgColor='gray.900' boxShadow='0px 0px 22px -3px rgba(0,0,0,0.8)' >
-        <Flex justifyContent='center' alignItems='center' gap={5}>
-          <Avatar name='Rodrigo Spano' src='https://bit.ly/ryan-florence' size='xl' />
-          <Link href='/cv.pdf' download color='orange.300' border='1px solid orange' p={2} borderRadius={10} display='flex' gap={1} alignItems='center' flexDirection='row' _hover={{color:'orange.400'}}  >
-            <Text>Download CV</Text>
-            <BsDownload />
-          </Link>
-        </Flex>
-        <Heading as='h2' color='gray.100' fontFamily='Comfortaa, cursive' textAlign='center' >
-          Rodrigo Spano
-        </Heading>
-        <Text color='gray.300' lineHeight={2} fontFamily='Comfortaa, cursive' fontSize='sm'>
-          lorem ipsu lorem ipsu lorem ipsu lorem ipsu lorem ipsu lorem ipsu lorem ipsu lorem ipsu lorem ipsu lorem ipsu lorem ipsu lorem ipsu lorem ipsu lorem ipsu lorem ipsu lorem ipsu lorem ipsu lorem ipsu lorem ipsu lorem ipsu lorem ipsu lorem ipsu lorem ipsu lorem ipsu lorem ipsu lorem ipsu lorem ipsu lorem ipsu lorem ipsu lorem ipsu lorem ipsu lorem ipsu lorem ipsu lorem ipsu lorem ipsu lorem ipsu lorem ipsu lorem ipsu lorem ipsu lorem ipsu lorem ipsu lorem ipsu lorem ipsu 
-        </Text>
+    <motion.div animate={animation}>
+      <Stack ref={ref} w='xl'>
+        <Heading as='h2' textAlign='center' fontFamily='Teko, sans-serif, cursive' color='orange.400' fontSize='6xl' >About me</Heading>
+        
+        <Stack p={5} borderRadius={20} bgColor='gray.900' boxShadow='0px 0px 22px -3px rgba(0,0,0,0.8)' >
+          <Flex justifyContent='center' alignItems='center' gap={5}>
+            <Avatar name='Rodrigo Spano' src='https://bit.ly/ryan-florence' size='xl' />
+            <Link href='/cv.pdf' download color='orange.300' border='1px solid orange' p={2} borderRadius={10} display='flex' gap={1} alignItems='center' flexDirection='row' _hover={{color:'orange.400'}}  >
+              <Text>Download CV</Text>
+              <BsDownload />
+            </Link>
+          </Flex>
+          <Heading as='h2' color='gray.100' fontFamily='Comfortaa, cursive' textAlign='center' >
+            Rodrigo Spano
+          </Heading>
+          <Text color='gray.300' lineHeight={2} fontFamily='Comfortaa, cursive' fontSize='sm'>
+            lorem ipsu lorem ipsu lorem ipsu lorem ipsu lorem ipsu lorem ipsu lorem ipsu lorem ipsu lorem ipsu lorem ipsu lorem ipsu lorem ipsu lorem ipsu lorem ipsu lorem ipsu lorem ipsu lorem ipsu lorem ipsu lorem ipsu lorem ipsu lorem ipsu lorem ipsu lorem ipsu lorem ipsu lorem ipsu lorem ipsu lorem ipsu lorem ipsu lorem ipsu lorem ipsu lorem ipsu lorem ipsu lorem ipsu lorem ipsu lorem ipsu lorem ipsu lorem ipsu lorem ipsu lorem ipsu lorem ipsu lorem ipsu lorem ipsu lorem ipsu 
+          </Text>
+
+        </Stack>
 
       </Stack>
-
-    </Stack>
+    </motion.div>
   )
 }
 
--- components/aboutme/Skills.jsx
+++ components/aboutme/Skills.jsx
@@ -1,11 +1,33 @@
-import { Stack } from '@chakra-ui/react';
-import React from 'react'
+import { Heading, Stack } from '@chakra-ui/react';
+import React, { useEffect } from 'react'
+import { useAnimation, motion } from 'framer-motion'
+import { useInView } from 'react-intersection-observer'
 
 function Skills() {
+
+  const { ref, inView } = useInView();
+  const animation = useAnimation();
+
+  useEffect(() => {
+    if(inView){
+      animation.start({
+        x:0,
+        transition:{type:'spring', bounce: 0.3, duration: 1, delay:0.2}
+      })
+    }
+    if(!inView){
+      animation.start({x:'-30vw'})
+    }
+  }, [inView])
+
   return (
-    <Stack bgColor='gray.300' h='xl' w='xs'>
-      
-    </Stack>
+    <motion.div ref={ref} animate={animation}>
+      <Stack h='xl' w='xs'>
+        <Heading as='h2' color='orange.400' fontFamily='Teko, sans-serif' fontSize='5xl' textAlign='center' >
+          Skills
+        </Heading>
+      </Stack>
+    </motion.div>
   )
 }
 
