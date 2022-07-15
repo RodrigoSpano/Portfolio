f36fa8ced617c7e6d5ab73c3c2bb6665d1d222ea
RodrigoSpano <rorrospano17@gmail.com>
2022-07-14 21:42:12 -0300

    fix: color

--- components/aboutme/SkillsCard.jsx
+++ components/aboutme/SkillsCard.jsx
@@ -5,7 +5,7 @@ function SkillsCard(props) {
   return (
     <Stack w={{base:'150px',md:'200px'}} h='50px' borderRadius={20} bg='gray.700' justifyContent='start' direction='row' alignItems='center' p={2} userSelect='none' boxShadow='0px 0px 22px -3px rgba(0,0,0,0.8)'  >
       <Image src={props.img} alt='technology image' boxSize='40px' borderRadius={10} objectFit='cover' />
-      <Heading as='h2' fontSize={{base:'sm',md:'xl'}} pt={5} fontFamily='Comfortaa, cursive' textTransform='uppercase' color='gray.800' >{props.name}</Heading>
+      <Heading as='h2' fontSize={{base:'sm',md:'xl'}} pt={4} fontFamily='Comfortaa, cursive' textTransform='uppercase' color='orange.400' >{props.name}</Heading>
     </Stack>
   )
 }
