b0693bc51ae22fb32ea6274037b66718b7858bed
Rodrigo Spano <rorrospano17@gmail.com>
2022-10-02 08:56:19 -0300

    saque proyecto no-country

--- components/projects/projectsDB.js
+++ components/projects/projectsDB.js
@@ -1,53 +1,45 @@
 const DB = [
   {
-    name:'appointment management app',
-    src:'/assets/pet-society.png',
-    link:'https://pet-society-lilac.vercel.app/',
-    technology:['Vite.js','Chakra-ui','redux','mongoDB'],
-    gh:'https://github.com/No-Country/c6-37-ft-mern.git'
+    name: 'Pricing Component',
+    src: '/assets/pricing.png',
+    link: 'https://pricing-component-rodrigo-spano.vercel.app/',
+    technology: ['next.js', 'react', 'chakra-ui', 'redux'],
+    gh: 'https://github.com/RodrigoSpano/Pricing-component-FrontEndMentor.git',
   },
   {
-    name:'Pricing Component',
-    src:'/assets/pricing.png',
-    link:'https://pricing-component-rodrigo-spano.vercel.app/',
-    technology:['next.js', 'react', 'chakra-ui','redux'],
-    gh:'https://github.com/RodrigoSpano/Pricing-component-FrontEndMentor.git',
-  },
-  {
-    name:'Fylo Landing',
-    src:'/assets/fylo-landing.png',
-    link:'https://fylo-landing-rodrigo-spano.vercel.app/',
-    technology:['next.js','react','chakra-ui'],
-    gh:'https://github.com/RodrigoSpano/Fylo-landing-frontEndMentor.git',
+    name: 'Fylo Landing',
+    src: '/assets/fylo-landing.png',
+    link: 'https://fylo-landing-rodrigo-spano.vercel.app/',
+    technology: ['next.js', 'react', 'chakra-ui'],
+    gh: 'https://github.com/RodrigoSpano/Fylo-landing-frontEndMentor.git',
   },
   {
     name: 'SneakersArg',
-    src:'/assets/allSneakers.png',
-    link:'https://sneakers-arg.netlify.app/',
-    technology:['vite','react','chakra-ui','firebase'],
-    gh:'https://github.com/RodrigoSpano/SneakersArg-Spano.git'
+    src: '/assets/allSneakers.png',
+    link: 'https://sneakers-arg.netlify.app/',
+    technology: ['vite', 'react', 'chakra-ui', 'firebase'],
+    gh: 'https://github.com/RodrigoSpano/SneakersArg-Spano.git',
   },
   {
-    name:'Weather App',
-    src:'/assets/weatherApp.png',
-    link:'https://weather-app-spano.vercel.app/',
-    technology:['next.js','react','react-ui','openWeather API'],
-    gh:'https://github.com/RodrigoSpano/Weather-app.git'
+    name: 'Weather App',
+    src: '/assets/weatherApp.png',
+    link: 'https://weather-app-spano.vercel.app/',
+    technology: ['next.js', 'react', 'react-ui', 'openWeather API'],
+    gh: 'https://github.com/RodrigoSpano/Weather-app.git',
   },
   {
     name: 'Task List',
-    src:'/assets/taskList.png',
-    link:'https://tasks-list-spano.netlify.app/',
-    technology:['vite','react','redux','chakra-ui'],
-    gh:'https://github.com/RodrigoSpano/Task-List.git'
+    src: '/assets/taskList.png',
+    link: 'https://tasks-list-spano.netlify.app/',
+    technology: ['vite', 'react', 'redux', 'chakra-ui'],
+    gh: 'https://github.com/RodrigoSpano/Task-List.git',
   },
   {
     name: 'Portfolio',
-    src:'/assets/portfolio.png',
-    link:'https://rodrigospano.vercel.app/',
-    technology:['next.js','react','chakra-ui','framer-motion'],
-    gh:'https://github.com/RodrigoSpano/Portfolio.git'
+    src: '/assets/portfolio.png',
+    link: 'https://rodrigospano.vercel.app/',
+    technology: ['next.js', 'react', 'chakra-ui', 'framer-motion'],
+    gh: 'https://github.com/RodrigoSpano/Portfolio.git',
   },
-
-]
+];
 export default DB;
